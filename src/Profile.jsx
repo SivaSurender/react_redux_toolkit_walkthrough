b01e452901429c96823d0204dde6de55ddcbd384
SivaSurender <e.surender14@gmail.com>
2024-06-04 19:15:09 +0530

    profile , login

---
+++ src/Profile.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Profile() {
+  return (
+    <div>
+      <h1>Name</h1>
+      <h1>Age</h1>
+      <h1>Location</h1>
+      <h1>Place</h1>
+    </div>
+  );
+}
+
+export default Profile;
